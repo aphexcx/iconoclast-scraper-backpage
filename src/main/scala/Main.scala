b0e36f537e6605f95f6755c68db24e0f4071d97f
Afik Cohen <aphex@aphex.cx>
2016-07-01 03:03:53 -0700

    Extract image URLs

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,21 @@
+import net.ruippeixotog.scalascraper.browser.JsoupBrowser
+import net.ruippeixotog.scalascraper.dsl.DSL.Extract._
+import net.ruippeixotog.scalascraper.dsl.DSL._
+import net.ruippeixotog.scalascraper.model.Element
+
+import scala.language.postfixOps
+
+/**
+  * Created by aphex on 7/1/16.
+  */
+object Main extends App {
+  val browser = JsoupBrowser()
+  val HOME = "http://sfbay.backpage.com/FemaleEscorts/two-girlsdouble-the-actioncall-if-your-alone-or-with-a-friend/33710143"
+  val doc = browser.get(HOME)
+
+  // Extract the image Urls
+  val imageUrls: List[String] = doc >> elementList("ul#viewAdPhotoLayout") >> elementList("li") flatMap (_ >> elementList("img")) flatMap (_ >> attr("src")("img"))
+
+    println()
+
+}
